e8981d0c727076bd46e04103bef6090bbf122d0f
Zhengyang Li <23238046@studentmail.ul.ie>
2025-03-06 12:30:12 +0000

    UI design

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.weatherforecast;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText cityInput;
+    TextView weatherInfo;
+    Button getWeatherButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        cityInput = findViewById(R.id.cityInput);
+        weatherInfo = findViewById(R.id.weatherInfo);
+        getWeatherButton = findViewById(R.id.getWeatherButton);
+
+        getWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = cityInput.getText().toString().trim();
+                // 之后在这里调用API获取天气数据
+                weatherInfo.setText("return city:" + city);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
