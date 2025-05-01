59c923fc89bed5383547518bb844103551f59ef4
Zhengyang Li <23238046@studentmail.ul.ie>
2025-05-01 16:42:16 +0100

    Replace with new project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,24 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "WeatherForecast"
+rootProject.name = "sunnyWeather"
 include(":app")
+ 
